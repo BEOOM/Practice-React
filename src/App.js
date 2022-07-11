8d6623bb22903ee02a5c5d3a08d6281f675cc243
BEOOM <wnsqja0512@naver.com>
2022-07-11 15:17:31 +0900

    5장 ref: DOM에 이름 달기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, {Component} from "react";
 import MyComponent from "./3장/MyComponent";
 import Counter from "./3장/Counter";
 import Say from "./3장/Say";
 import EventPracticeClass from "./4장/EventPracticeClass";
 import EventPracticeFn from "./4장/EventPracticeFn";
 import EventPracticeFn2 from "./4장/EventPracticeFn";
+import ValidationSample from "./5장/ValidationSample";
+import ScrollBox from "./5장/ScrollBox";
 
-function App() {
-  return <EventPracticeFn2 />;
+// function App() {
+//   return (
+//     <div>
+//       <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+//       <button onClick={() => this.scrollBox.scrollToBottom()}>맨밑으로</button>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render(){
+  return(
+    <div>
+    <ScrollBox ref={ref => this.scrollBox=ref}/>
+    <button onClick={() => this.scrollBox.scrollToBottom()}>밑으로</button>
+    </div>
+  )
+  }
 }
 
 export default App;
